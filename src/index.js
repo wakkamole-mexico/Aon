4a2dca12c731b92a30ea6578a3bc43df9f11e1bc
CarlosRuiz <ingecarlosrm@gmail.com>
2021-04-23 13:30:58 -0500

    [ADD] eslint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import HelloWorld from './components/hellowWorld';
+import HelloWorld from './components/HellowWorld';
 
 
 
-ReactDOM.render(<HelloWorld/>, document.getElementById(app));
+ReactDOM.render(<HelloWorld/>, document.getElementById('app'));
