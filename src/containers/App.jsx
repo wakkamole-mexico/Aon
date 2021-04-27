7902a78a50cfeaa697b26039b8da1763a26286fd
CarlosRuiz <ingecarlosrm@gmail.com>
2021-04-27 15:48:23 -0500

    [ADD] img config webpack

---
+++ src/containers/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Menu from '../components/Menu';
+import '../assets/styles/App.scss'
+
+const App = () => (
+  <div className='App'>
+    <Menu />
+  </div>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Menu from './components/Menu';
+import App from './containers/App';
 
-
-
-ReactDOM.render(<Menu/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
