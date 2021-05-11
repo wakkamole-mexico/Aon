67e4b8199be6028a3c9bb964a0807d2c8565ac27
CarlosRuiz <ingecarlosrm@gmail.com>
2021-05-11 12:30:57 -0500

    [UPDATE] styles header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,22 +5,24 @@ import HeaderImage from '../assets/static/image-home.png';
 
 const Home = () => (
   <div className='home u-wrapper'>
-    <div className='logo'>
-      <div className='logo__container'>
-        <img src={logoAon} alt='' />
+    <div className='home__container'>
+      <div className='logo'>
+        <div className='logo__container'>
+          <img src={logoAon} alt='' />
+        </div>
       </div>
-    </div>
-    <div className='header'>
-      <div className='header__text'>
-        <h1>Comprometidos en potenciar las posibilidades económicas y humanas</h1>
-        <p>Cuidamos el valor de tu empresa. En Aon, tenemos el conocimiento, la experiencia y el compromiso para poder ofrecer a nuestros clientes un servicio con valor agregado acompañado de un enfoque de especialistas en el tema. Ofrecemos soluciones innovadoras en el mercado. Conoce más.</p>
+      <div className='header'>
+        <div className='header__text'>
+          <h1>Comprometidos en potenciar las posibilidades económicas y humanas</h1>
+          <p>Cuidamos el valor de tu empresa. En Aon, tenemos el conocimiento, la experiencia y el compromiso para poder ofrecer a nuestros clientes un servicio con valor agregado acompañado de un enfoque de especialistas en el tema. Ofrecemos soluciones innovadoras en el mercado. Conoce más.</p>
+        </div>
+        <div className='header__image'>
+          <img src={HeaderImage} alt="" />
+        </div>
       </div>
-      <div className='header__image'>
-        <img src={HeaderImage} alt="" />
+      <div className="cta__container">
+        <a className="item--cta" href="">Empezar</a>
       </div>
-    </div>
-    <div className="cta__container">
-      <a className="item--cta" href="">Empezar</a>
     </div>
   </div>
 );
