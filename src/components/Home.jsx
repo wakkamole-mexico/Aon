8827f33c4e82ea96866d0f5ebe6e30d7b86c1124
CarlosRuiz <ingecarlosrm@gmail.com>
2021-05-11 12:16:51 -0500

    [UPDATE] new header & image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../assets/styles/components/Home.scss';
 import logoAon from '../assets/static/logo-Aon.png';
-import HeaderImage from '../assets/static/header.png';
+import HeaderImage from '../assets/static/image-home.png';
 
 const Home = () => (
   <div className='home u-wrapper'>
@@ -11,9 +11,16 @@ const Home = () => (
       </div>
     </div>
     <div className='header'>
-      <div className='header__imagen'>
+      <div className='header__text'>
+        <h1>Comprometidos en potenciar las posibilidades económicas y humanas</h1>
+        <p>Cuidamos el valor de tu empresa. En Aon, tenemos el conocimiento, la experiencia y el compromiso para poder ofrecer a nuestros clientes un servicio con valor agregado acompañado de un enfoque de especialistas en el tema. Ofrecemos soluciones innovadoras en el mercado. Conoce más.</p>
       </div>
-      <p>Cuidamos el valor de tu empresa. En Aon, tenemos el conocimiento, la experiencia y el compromiso para poder ofrecer a nuestros clientes un servicio con valor agregado acompañado de un enfoque de especialistas en el tema. Ofrecemos soluciones innovadoras en el mercado. Conoce más.</p>
+      <div className='header__image'>
+        <img src={HeaderImage} alt="" />
+      </div>
+    </div>
+    <div className="cta__container">
+      <a className="item--cta" href="">Empezar</a>
     </div>
   </div>
 );
