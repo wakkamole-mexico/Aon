21d211686d2a74065180fe8be7a3ef546c0ff0c3
CarlosRuiz <ingecarlosrm@gmail.com>
2021-05-26 12:36:53 -0500

    [ADD] component energia

---
+++ src/components/Energia.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Energia = () => {
+  return (
+    <div />
+  );
+};
+
+export default Energia;
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink,
+} from 'react-router-dom';
 import energiaOp from '../assets/static/energia-op.svg';
 import foodOp from '../assets/static/foodop.svg';
 import farmOp from '../assets/static/farmop.svg';
@@ -6,60 +13,64 @@ import toolsOp from '../assets/static/toolsop.svg';
 import airOp from '../assets/static/airop.svg';
 import '../assets/styles/components/Menu.scss';
 
-const Menu = () => (
-  <div className='menu u-wrapper' id='menu'>
-    <div className='menu__container'>
-      <div className='indutry #1'>
-        <div className='industry__icon'>
-          <img src={energiaOp} alt='' />
-        </div>
-        <div className='industry__title title__focus'>
-          <p>Energía</p>
+function Menu() {
+  return (
+
+      <div className='menu u-wrapper' id='menu'>
+        <div className='menu__container'>
+          <div className='indutry #1'>
+            <div className='industry__icon'>
+              <img src={energiaOp} alt='' />
+            </div>
+            <div className='industry__title title__focus'>
+              <p>Energía</p>
+            </div>
+          </div>
+          <div className='indutry #2'>
+            <div className='industry__icon'>
+              <img src={foodOp} alt='' />
+            </div>
+            <div className='industry__title'>
+              <p>
+                Alimentos y
+                bebidas
+              </p>
+            </div>
+          </div>
+          <div className='indutry #3'>
+            <div className='industry__icon'>
+              <img src={farmOp} alt='' />
+            </div>
+            <div className='industry__title'>
+              <p>
+                Farmacéutica y
+                química
+              </p>
+            </div>
+          </div>
+          <div className='indutry #4'>
+            <div className='industry__icon'>
+              <img src={toolsOp} alt='' />
+            </div>
+            <div className='industry__title'>
+              <p>Manufactura</p>
+            </div>
+          </div>
+          <div className='indutry #5'>
+            <div className='industry__icon'>
+              <img src={airOp} alt='' />
+            </div>
+            <div className='industry__title'>
+              <p>
+                Transporte y
+                logística
+              </p>
+            </div>
+          </div>
         </div>
       </div>
-      <div className='indutry #2'>
-        <div className='industry__icon'>
-          <img src={foodOp} alt='' />
-        </div>
-        <div className='industry__title'>
-          <p>
-            Alimentos y
-            bebidas
-          </p>
-        </div>
-      </div>
-      <div className='indutry #3'>
-        <div className='industry__icon'>
-          <img src={farmOp} alt='' />
-        </div>
-        <div className='industry__title'>
-          <p>
-            Farmacéutica y
-            química
-          </p>
-        </div>
-      </div>
-      <div className='indutry #4'>
-        <div className='industry__icon'>
-          <img src={toolsOp} alt='' />
-        </div>
-        <div className='industry__title'>
-          <p>Manufactura</p>
-        </div>
-      </div>
-      <div className='indutry #5'>
-        <div className='industry__icon'>
-          <img src={airOp} alt='' />
-        </div>
-        <div className='industry__title'>
-          <p>
-            Transporte y
-            logística
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-);
+
+  );
+};
 
 export default Menu;
